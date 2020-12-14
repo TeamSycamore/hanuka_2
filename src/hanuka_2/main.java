03abd3973ba04d8b2e93edceaf975b3f7300c9ad
TeamSycamore <teamguy5614@gmail.com>
2020-12-14 05:54:59 -0800

    all the 3 arrs in one

--- src/hanuka_2/main.java
+++ src/hanuka_2/main.java
@@ -7,106 +7,101 @@ import java.util.Scanner;
 public class main {
 
 	public static void main(String[] args) {
-		
-		
-//		 Scanner myObj = new Scanner(System.in);  // Create a Scanner object
-//		 System.out.println("Enter username");
-//
-//		 String userName = myObj.nextLine();  // Read user input
-//
-//		 System.out.println("Username is: " + userName);  // Output user input
-		 int[] time = {1,2,2,4,5,2,5};
-		 hanuka2(7,time);
+
+
+		//		 Scanner myObj = new Scanner(System.in);  // Create a Scanner object
+		//		 System.out.println("Enter username");
+		//
+		//		 String userName = myObj.nextLine();  // Read user input
+		//
+		//		 System.out.println("Username is: " + userName);  // Output user input
+		int[] time = {1,2,2,4,5,2,5};
+		hanuka2(7,time);
 	}
-	
+
 	public static void hanuka2(int n, int[] time) {
 		int yak = 0;
 		Arrays.sort(time);
 		int[][] cluser = new int[n][3];
 		int[] gushim = new int[n]; // clusters
 		int[] optTime = new int[n]; // Recommended time
-		
+
 		//~~~~~~~~~~~~~~~ creating cluster arr ~~~~~~~~~~~~
-		int curr = time[0];
 		cluser[0][0]=time[0];
 		int ind = 0;
 		cluser[ind][1]++;
-		gushim[ind]++;
 		for (int i = 1; i < time.length; i++) {
-			if(curr == time[i]){
-				gushim[ind]++;
-			cluser[ind][1]++;}
+			if(cluser[ind][0] == time[i]){
+
+				cluser[ind][1]++;}
 			else {				
-				curr = time[i];
+
 				ind++;
-				gushim[ind]++;
-				cluser[ind][0]=curr;
+				cluser[ind][0]= time[i];
 				cluser[ind][1]++;
-				
+
 			}
 		}
-		
+
 		//~~~~~~~~~~~~~~~ creating optimal time arr ~~~~~~~~~~~~
-		optTime[0] = 1;
 		cluser[0][2]=1;
-		int num = gushim[0] ;
-		for (int i = 1; i < gushim.length; i++) {
-			if(gushim[i] > 0 ) {
-				optTime[i] = time[num] - gushim[i]/2;
-				cluser[i][2]=time[num] - gushim[i]/2;
-				num = num + gushim[i];
+		int num = cluser[0][1] ;
+		for (int i = 1; i < cluser.length; i++) {
+			if(cluser[i][1] > 0 ) {
+				cluser[i][2]=time[num] - cluser[i][1]/2;
+				num = num + cluser[i][1];
 			}
-			
+
 		}
-				System.out.println("gushim: "+Arrays.toString(gushim));
-				System.out.println("time: "+Arrays.toString(time));
-				System.out.println("op: "+Arrays.toString(optTime));
-				System.out.println("c: ");
-				for (int[] i : cluser) {
-					System.out.println(" "+Arrays.toString(i));
-				}
-				
+		System.out.println("gushim: "+Arrays.toString(gushim));
+		System.out.println("time: "+Arrays.toString(time));
+		System.out.println("op: "+Arrays.toString(optTime));
+		System.out.println("c: ");
+		for (int[] i : cluser) {
+			System.out.println(" "+Arrays.toString(i));
+		}
+
 	}
-//		for (int element: optTime) {
-//            System.out.println(element);
-//        }
-		
-//		
-//		
-//		//~~~~~~~~~~~~~~~ finding 
-//		for (int i = 0; i < gushim.length - 1; i++) {
-//			if(gushim[i] + optTime[i] > optTime[i+1]) {
-//				boolean flag = true;
-//				int j = i;
-//				int[] cluster = new int[n];
-//				for (int k = 0; k < gushim.length - 1; k++) {
-//					cluster[k] = gushim[j];
-//					if(gushim[i] + optTime[i] < optTime[i+1]) {
-//						i = j + 1;
-//						break;
-//					}
-//					j++;
-//				}
-//				
-//			}
-//			else {
-//				if(gushim[i] == 2)
-//					yak = yak + 1;
-//				if(gushim[i] != 1) {
-//					if(gushim[i] % 2 == 1) {
-//						int j = (gushim[i] - 1)/2;
-//						yak = (int) (yak + (j + Math.pow(j, 2)));
-//					}  
-//					if(gushim[i] % 2 == 0) {
-//						int j = (gushim[i] - 1)/2;
-//						yak = (int) (yak + (j + Math.pow(j, 2))) - j;
-//					}
-//				}
-//				
-//			}
-//		}
-//		System.out.println(yak);
-//	}
+	//		for (int element: optTime) {
+	//            System.out.println(element);
+	//        }
+
+	//		
+	//		
+	//		//~~~~~~~~~~~~~~~ finding 
+	//		for (int i = 0; i < gushim.length - 1; i++) {
+	//			if(gushim[i] + optTime[i] > optTime[i+1]) {
+	//				boolean flag = true;
+	//				int j = i;
+	//				int[] cluster = new int[n];
+	//				for (int k = 0; k < gushim.length - 1; k++) {
+	//					cluster[k] = gushim[j];
+	//					if(gushim[i] + optTime[i] < optTime[i+1]) {
+	//						i = j + 1;
+	//						break;
+	//					}
+	//					j++;
+	//				}
+	//				
+	//			}
+	//			else {
+	//				if(gushim[i] == 2)
+	//					yak = yak + 1;
+	//				if(gushim[i] != 1) {
+	//					if(gushim[i] % 2 == 1) {
+	//						int j = (gushim[i] - 1)/2;
+	//						yak = (int) (yak + (j + Math.pow(j, 2)));
+	//					}  
+	//					if(gushim[i] % 2 == 0) {
+	//						int j = (gushim[i] - 1)/2;
+	//						yak = (int) (yak + (j + Math.pow(j, 2))) - j;
+	//					}
+	//				}
+	//				
+	//			}
+	//		}
+	//		System.out.println(yak);
+	//	}
 
 }
 
